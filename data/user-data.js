4d1f497ccbdb5e007ff0b69ca8954791297c8763
tuanhao2003 <nhtuanhao@gmail.com>
2025-03-02 23:03:52 +0700

    init

---
+++ data/user-data.js
@@ -0,0 +1,13 @@
+export const userData = {
+  githubUser: 'tuanhao2003',
+  devUsername: "nhtuanhao",
+  github: 'https://github.com/tuanhao2003',
+  facebook: '',
+  linkedIn: '',
+  twitter: '',
+  stackOverflow: '',
+  leetcode: "",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+  skills: ['React', 'SpringBoot', 'Java', '.NET', 'Python', 'PHP','MySql', 'Postgres', 'Docker', 'AWS'],
+  timezone: '+7'
+};
