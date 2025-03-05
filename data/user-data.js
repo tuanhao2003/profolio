8acc87a1767625ba3b12447cd4b12373fd5c6546
tuanhao2003 <136846608+tuanhao2003@users.noreply.github.com>
2025-03-05 15:11:45 +0700

    Update user-data.js

--- data/user-data.js
+++ data/user-data.js
@@ -6,7 +6,7 @@ export const userData = {
   linkedIn: '',
   twitter: '',
   stackOverflow: '',
-  leetcode: "",
+  leetcode: '',
   resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
   skills: ['React', 'SpringBoot', 'Java', '.NET', 'Python', 'PHP','MySql', 'Postgres', 'Docker', 'AWS'],
   timezone: '+7'
